eecc339b7d076ff7df2f229e2d03bf00da4ae67a
happybay <net.or.th@gmail.com>
2024-05-29 22:34:05 +0700

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    testnet: {
+      url: 'https://data-seed-prebsc-2-s3.bnbchain.org:8545/',
+      accounts: [`${process.env.PRIVATE_KEY}`] // Replace with your private key, securely managed
+    }
+  }
 };
